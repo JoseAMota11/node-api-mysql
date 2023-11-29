52d76dc8310dfa008626bc8c2213f8cb0b833ff9
JoseAMota11 <jose.a.mota11@gmail.com>
2023-11-29 11:15:12 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Response } from 'express';
+import 'dotenv/config';
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+app.get('/api', (_, res: Response) => {
+  res.json({ message: "I'm alive!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
