6832bda0c1b84a565af783452035fde5c8622ff0
JoseAMota11 <jose.a.mota11@gmail.com>
2023-11-29 11:47:23 -0400

    Adding more options to the connection

--- src/db/todos.db.ts
+++ src/db/todos.db.ts
@@ -6,4 +6,11 @@ export const connection = mysql.createConnection({
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DATABASE,
   password: process.env.MYSQL_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
