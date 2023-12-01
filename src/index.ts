ba4ae170678fef55a0d169e801c5a2c862dfd386
JoseAMota11 <jose.a.mota11@gmail.com>
2023-12-01 12:30:56 -0400

    Configuring CORS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
 import todoRoutes from './routes/todos.routes';
+import cors, { CorsOptions } from 'cors';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+
+const corsOptions: CorsOptions = {
+  origin: process.env.URL_ORIGIN,
+};
+
+// Middlewares
+app.use(cors(corsOptions));
 
 app.disable('x-powered-by');
 app.use(express.json());
