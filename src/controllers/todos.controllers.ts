d8a93ba5b06e28cd8f321eb3788bf75a754d1b64
JoseAMota11 <jose.a.mota11@gmail.com>
2023-12-05 11:45:49 -0400

    Improving error handling

--- src/controllers/todos.controllers.ts
+++ src/controllers/todos.controllers.ts
@@ -5,76 +5,99 @@ import { Todo } from '../types/todos';
 const getTodos = (_: Request, res: Response) => {
   TodoModels.selectTodos()
     .then((result) => {
-      res.json(result);
+      if (result) {
+        res.json(result);
+      } else {
+        throw new Error("Could not get TO-DO's from the database.");
+      }
     })
-    .catch((err) => {
-      throw err;
+    .catch((err: Error) => {
+      res.status(500).json({ error: err.message });
     });
 };
 
 const getOneTodo = (req: Request, res: Response) => {
   TodoModels.selectOneTodo(req.params.id)
     .then((result) => {
-      if (result.length === 0) {
-        res
-          .status(404)
-          .json({ message: `There's no todo with ID (${req.params.id}).` });
+      if (result) {
+        if (result.length === 0) {
+          res
+            .status(404)
+            .json({ message: `There's no todo with ID (${req.params.id}).` });
+        } else {
+          res.json(result);
+        }
       } else {
-        res.json(result);
+        throw new Error('Could not get TO-DO from the database.');
       }
     })
-    .catch((err) => {
-      throw err;
+    .catch((err: Error) => {
+      res.status(500).json({ error: err.message });
     });
 };
 
 const createTodo = (req: Request, res: Response) => {
   TodoModels.insertTodo(req.body as Todo)
-    .then(({ affectedRows, insertId }) => {
-      if (affectedRows === 1) {
-        res.json({
-          message: `A new todo has been created with ID (${insertId}).`,
-        });
+    .then((result) => {
+      if (result) {
+        const { affectedRows, insertId } = result;
+        if (affectedRows === 1) {
+          res.json({
+            message: `A new todo has been created with ID (${insertId}).`,
+          });
+        }
+      } else {
+        throw new Error('Could not create TO-DO.');
       }
     })
-    .catch((err) => {
-      throw err;
+    .catch((err: Error) => {
+      res.status(500).json({ error: err.message });
     });
 };
 
 const updateTodo = (req: Request, res: Response) => {
   TodoModels.updateTodo(req.body as Todo, req.params.id as Todo['id'])
-    .then(({ affectedRows }) => {
-      if (affectedRows === 1) {
-        res.json({
-          message: `A todo with ID (${req.params.id}) has been updated.`,
-        });
+    .then((result) => {
+      if (result) {
+        const { affectedRows } = result;
+        if (affectedRows === 1) {
+          res.json({
+            message: `A todo with ID (${req.params.id}) has been updated.`,
+          });
+        } else {
+          res.status(404).json({
+            message: `There's no todo with ID (${req.params.id}).`,
+          });
+        }
       } else {
-        res.status(404).json({
-          message: `There's no todo with ID (${req.params.id}).`,
-        });
+        throw new Error('Could not update TO-DO.');
       }
     })
-    .catch((err) => {
-      throw err;
+    .catch((err: Error) => {
+      res.status(500).json({ error: err.message });
     });
 };
 
 const deleteTodo = (req: Request, res: Response) => {
   TodoModels.deleteTodo(req.params.id as Todo['id'])
-    .then(({ affectedRows }) => {
-      if (affectedRows === 1) {
-        res.json({
-          message: `A todo with ID (${req.params.id}) has been deleted.`,
-        });
+    .then((result) => {
+      if (result) {
+        const { affectedRows } = result;
+        if (affectedRows === 1) {
+          res.json({
+            message: `A todo with ID (${req.params.id}) has been deleted.`,
+          });
+        } else {
+          res.status(404).json({
+            message: `There's no todo with ID (${req.params.id}).`,
+          });
+        }
       } else {
-        res.status(404).json({
-          message: `There's no todo with ID (${req.params.id}).`,
-        });
+        throw new Error('Could not delete TO-DO.');
       }
     })
-    .catch((err) => {
-      throw err;
+    .catch((err: Error) => {
+      res.status(500).json({ error: err.message });
     });
 };
 
--- src/models/todos.models.ts
+++ src/models/todos.models.ts
@@ -3,43 +3,73 @@ import type { ResultSetHeader } from 'mysql2';
 import type { Todo } from '../types/todos';
 
 const selectTodos = async () => {
-  const [rows] = await pool.query('SELECT * FROM todos;');
+  try {
+    const [rows] = await pool.query('SELECT * FROM todos;');
 
-  return rows as Todo[];
+    return rows as Todo[];
+  } catch (error) {
+    console.error(error);
+  } finally {
+    pool.releaseConnection(await pool.getConnection());
+  }
 };
 
 const selectOneTodo = async (id: string) => {
-  const [rows] = await pool.execute('SELECT * FROM `todos` WHERE `id` = ?;', [
-    id,
-  ]);
+  try {
+    const [rows] = await pool.execute('SELECT * FROM `todos` WHERE `id` = ?;', [
+      id,
+    ]);
 
-  return rows as Todo[];
+    return rows as Todo[];
+  } catch (error) {
+    console.error(error);
+  } finally {
+    pool.releaseConnection(await pool.getConnection());
+  }
 };
 
 const insertTodo = async ({ title, description }: Todo) => {
-  const [rows] = await pool.execute(
-    'INSERT INTO `todos` (`title`, `description`) VALUES (?, ?);',
-    [title, description]
-  );
+  try {
+    const [rows] = await pool.execute(
+      'INSERT INTO `todos` (`title`, `description`) VALUES (?, ?);',
+      [title, description]
+    );
 
-  return rows as ResultSetHeader;
+    return rows as ResultSetHeader;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    pool.releaseConnection(await pool.getConnection());
+  }
 };
 
 const updateTodo = async ({ title, description }: Todo, id: Todo['id']) => {
-  const [rows] = await pool.execute(
-    'UPDATE `todos` SET `title` = ?, `description` = ? WHERE `id` = ?;',
-    [title, description, id]
-  );
+  try {
+    const [rows] = await pool.execute(
+      'UPDATE `todos` SET `title` = ?, `description` = ? WHERE `id` = ?;',
+      [title, description, id]
+    );
 
-  return rows as ResultSetHeader;
+    return rows as ResultSetHeader;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    pool.releaseConnection(await pool.getConnection());
+  }
 };
 
 const deleteTodo = async (id: Todo['id']) => {
-  const [rows] = await pool.execute('DELETE FROM `todos` WHERE `id` = ?;', [
-    id,
-  ]);
+  try {
+    const [rows] = await pool.execute('DELETE FROM `todos` WHERE `id` = ?;', [
+      id,
+    ]);
 
-  return rows as ResultSetHeader;
+    return rows as ResultSetHeader;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    pool.releaseConnection(await pool.getConnection());
+  }
 };
 
 export const TodoModels = {
